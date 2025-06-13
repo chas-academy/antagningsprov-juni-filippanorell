bf56803319cc91c7ac045a7cd6a0c450eecb9ff1
filippanorell <145430464+filippanorell@users.noreply.github.com>
2025-06-13 09:18:46 +0200

    Löste uppgift 5

--- uppg5.js
+++ uppg5.js
@@ -1,11 +1,12 @@
-
 function uppg5() {
-    // skriv en if-sats som jämför två tal
-    // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
-    // annars skriv ut "Det andra talet är större än det första"
+  const num1 = 10;
+  const num2 = 5;
 
-
-
+  if (num1 > num2) {
+    console.log("Det första talet är större än det andra");
+  } else {
+    console.log("Det andra talet är större än det första");
+  }
 }
 
 module.exports = { uppg5 };
