ea5f97782c25b887717b7c427a068c49fadc787a
filippanorell <145430464+filippanorell@users.noreply.github.com>
2025-06-13 09:23:35 +0200

    Löste uppgift 9

--- uppg9.js
+++ uppg9.js
@@ -1,14 +1,17 @@
+function uppg9() {
+  function sort(arr) {
+    arr.forEach((num) => {
+      if (num % 2 === 0) {
+        console.log(`${num} jämt`);
+      } else {
+        console.log(`${num} udda`);
+      }
+    });
+  }
 
-
-function uppg9 (){
-    // skapa en funktion som heter sort och tar in en array av siffror som argument
-    // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
-    // i if-sattsen skriv ut i konsolen resultatet
-    // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
-
-    // anropa funktionen och skicka med en array som argument
-
-  
+  // Exempelarray
+  const numbers = [1, 2, 3, 4, 5, 6];
+  sort(numbers);
 }
 
 module.exports = { uppg9 };
