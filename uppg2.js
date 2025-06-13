7ef9bc4c198be03ee2b1616ffc686846ee3b1735
filippanorell <145430464+filippanorell@users.noreply.github.com>
2025-06-13 09:14:46 +0200

    Löste uppgift 2

--- uppg2.js
+++ uppg2.js
@@ -1,9 +1,6 @@
-
 function uppg2() {
-   // Skriv din kod här
-   // deklarera en variable med ditt namn och skriv ut det i konsolen
-
-
+  const namn = "Filippa";
+  console.log(namn);
 }
 
 module.exports = { uppg2 };
