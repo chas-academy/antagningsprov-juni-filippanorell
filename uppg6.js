7c001f53a180f442ee16f43e60def1508df0d6e0
filippanorell <145430464+filippanorell@users.noreply.github.com>
2025-06-13 09:19:39 +0200

    Löste uppgift 6

--- uppg6.js
+++ uppg6.js
@@ -1,11 +1,9 @@
+function uppg6() {
+  const names = ["Anna", "Filippa", "Kalle", "Sara"];
 
-
-function uppg6(){
-
-    // Skapa en array med namn som du döper till names
-    // Loopa igenom arrayen och skriv ut alla namn i konsolen
-
- 
+  for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+  }
 }
 
 module.exports = { uppg6 };
